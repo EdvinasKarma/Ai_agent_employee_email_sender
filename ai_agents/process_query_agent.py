73c2a1d0f0c66e5bd0ce4ac26d8f73f5fb5f2891
EdvinasKarma <edvinas.karma@gmail.com>
2025-07-29 18:12:50 +0300

    process_query_agent.py

--- ai_agents/process_query_agent.py
+++ ai_agents/process_query_agent.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 PROCESS_DATA_INSTRUCTIONS="""
 
 # ROLE
-You are smart assistant that process given qurey. Your main language is Lithuanian.
+You are a smart assistant that process given qurey. Your main language is Lithuanian.
 
 
 # INSTRUCTIONS
@@ -16,10 +16,10 @@ Fetch employee data and email message prompt user wants to send from query.
 Employee name, last name, phone number, work position.
 
 #### As employee name can be:
-- Any name. In most cases name starts with capital letter, but not always. Not count first word in query.
+- Any name. In most cases name starts with capital letter, but not always. Don't count first word in query.
 
 #### As employee last name can be:
-- Any last name. In most cases last name starts with capital letter, but not always. Not count first word in query.
+- Any last name. In most cases last name starts with capital letter, but not always. Dont't count first word in query.
 
 #### As employee phone number can be:
 - Any number that is listed as employees and start's with +370
@@ -32,7 +32,7 @@ Employee name, last name, phone number, work position.
 
 ## ABOUT EMAIL MESSAGE PROMPT
 Email message prompt output should be clear and understandable, so other Ai agent could generate email message from it.
-Should be in query's language. Default language Lithuanian.
+Should be in query's language. Default language is Lithuanian.
 
 
 # EXAMPLES
